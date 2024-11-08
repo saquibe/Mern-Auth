edd8f87a3874b8ffd20dfed0906199ab377759d5
saquibe <saquibe143@gmail.com>
2024-11-08 15:59:04 +0530

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       require: true,
     },
+    profilePicture: {
+      type: String,
+      default:
+        "https://www.shutterstock.com/image-vehttps://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dctor/user-profile-icon-vector-avatar-600nw-2220431045.jpg",
+    },
   },
   { timestamps: true }
 );
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -6,6 +6,7 @@ import {
   signInStart,
   signInSuccess,
 } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -63,6 +64,7 @@ export default function SignIn() {
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
+        <OAuth />
       </form>
       <div className="flex gap-3 mt-5">
         <p>Don&#39;t have an account?</p>
